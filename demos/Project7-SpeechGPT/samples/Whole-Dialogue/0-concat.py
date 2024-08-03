0c1a4682162ba04f01d79538cdb811d5c75593b4
jinny1208 <jeontech@gmail.com>
2024-08-03 13:45:35 +0900

    final

--- demos/Project7-SpeechGPT/samples/Whole-Dialogue/0-concat.py
+++ demos/Project7-SpeechGPT/samples/Whole-Dialogue/0-concat.py
@@ -1,16 +1,16 @@
 import wave
 
-# infiles = ["Proposed/D59/0_0_d59.wav", "Proposed/D59/1_1_d59.wav", "Proposed/D59/2_0_d59.wav", "Proposed/D59/3_1_d59.wav", "Proposed/D59/4_0_d59.wav", "Proposed/D59/5_1_d59.wav", "Proposed/D59/6_0_d59.wav", "Proposed/D59/7_1_d59.wav"]
-# outfile = "Proposed/D59-proposed-all.wav"
+# infiles = ["Baseline-noHistory/D59/0_0_d59.wav", "Baseline-noHistory/D59/1_1_d59.wav", "Baseline-noHistory/D59/2_0_d59.wav", "Baseline-noHistory/D59/3_1_d59.wav", "Baseline-noHistory/D59/4_0_d59.wav", "Baseline-noHistory/D59/5_1_d59.wav", "Baseline-noHistory/D59/6_0_d59.wav", "Baseline-noHistory/D59/7_1_d59.wav"]
+# outfile = "Baseline-noHistory/D59-Baseline-noHistory-all.wav"
 
-# infiles = ["Proposed/D64/0_0_d64.wav", "Proposed/D64/1_1_d64.wav", "Proposed/D64/2_0_d64.wav", "Proposed/D64/3_1_d64.wav", "Proposed/D64/4_0_d64.wav", "Proposed/D64/5_1_d64.wav", "Proposed/D64/6_0_d64.wav", "Proposed/D64/7_1_d64.wav", "Proposed/D64/8_0_d64.wav", "Proposed/D64/9_1_d64.wav", "Proposed/D64/10_0_d64.wav", "Proposed/D64/11_1_d64.wav", "Proposed/D64/12_0_d64.wav", "Proposed/D64/13_1_d64.wav"]
-# outfile = "Proposed/D64-proposed-all.wav"
+# infiles = ["Baseline-noHistory/D64/0_0_d64.wav", "Baseline-noHistory/D64/1_1_d64.wav", "Baseline-noHistory/D64/2_0_d64.wav", "Baseline-noHistory/D64/3_1_d64.wav", "Baseline-noHistory/D64/4_0_d64.wav", "Baseline-noHistory/D64/5_1_d64.wav", "Baseline-noHistory/D64/6_0_d64.wav", "Baseline-noHistory/D64/7_1_d64.wav", "Baseline-noHistory/D64/8_0_d64.wav", "Baseline-noHistory/D64/9_1_d64.wav", "Baseline-noHistory/D64/10_0_d64.wav", "Baseline-noHistory/D64/11_1_d64.wav", "Baseline-noHistory/D64/12_0_d64.wav", "Baseline-noHistory/D64/13_1_d64.wav"]
+# outfile = "Baseline-noHistory/D64-Baseline-noHistory-all.wav"
 
-infiles = ["Proposed/D71/0_1_d71.wav", "Proposed/D71/1_0_d71.wav", "Proposed/D71/2_1_d71.wav", "Proposed/D71/3_0_d71.wav", "Proposed/D71/4_1_d71.wav", "Proposed/D71/5_0_d71.wav"]
-outfile = "Proposed/D71-proposed-all.wav"
+# infiles = ["Baseline-noHistory/D71/0_1_d71.wav", "Baseline-noHistory/D71/1_0_d71.wav", "Baseline-noHistory/D71/2_1_d71.wav", "Baseline-noHistory/D71/3_0_d71.wav", "Baseline-noHistory/D71/4_1_d71.wav", "Baseline-noHistory/D71/5_0_d71.wav"]
+# outfile = "Baseline-noHistory/D71-Baseline-noHistory-all.wav"
 
-# infiles = ["Proposed/D79/0_1_d79.wav", "Proposed/D79/1_0_d79.wav", "Proposed/D79/2_1_d79.wav", "Proposed/D79/3_0_d79.wav", "Proposed/D79/4_1_d79.wav", "Proposed/D79/5_0_d79.wav", "Proposed/D79/6_1_d79.wav", "Proposed/D79/7_0_d79.wav", "Proposed/D79/8_1_d79.wav", "Proposed/D79/9_0_d79.wav", "Proposed/D79/10_1_d79.wav", "Proposed/D79/11_0_d79.wav", "Proposed/D79/12_1_d79.wav"]
-# outfile = "Proposed/D79-proposed-all.wav"
+infiles = ["Baseline-noHistory/D79/0_1_d79.wav", "Baseline-noHistory/D79/1_0_d79.wav", "Baseline-noHistory/D79/2_1_d79.wav", "Baseline-noHistory/D79/3_0_d79.wav", "Baseline-noHistory/D79/4_1_d79.wav", "Baseline-noHistory/D79/5_0_d79.wav", "Baseline-noHistory/D79/6_1_d79.wav", "Baseline-noHistory/D79/7_0_d79.wav", "Baseline-noHistory/D79/8_1_d79.wav", "Baseline-noHistory/D79/9_0_d79.wav", "Baseline-noHistory/D79/10_1_d79.wav", "Baseline-noHistory/D79/11_0_d79.wav", "Baseline-noHistory/D79/12_1_d79.wav"]
+outfile = "Baseline-noHistory/D79-Baseline-noHistory-all.wav"
 
 data= []
 for infile in infiles:
