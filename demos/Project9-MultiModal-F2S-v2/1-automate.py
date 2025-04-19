17f0e3a14cd5541a403ae5713343a8080893689e
jinny1208 <jeontech@gmail.com>
2025-04-19 17:46:21 +0900

    a lot of changes

--- demos/Project9-MultiModal-F2S-v2/1-automate.py
+++ demos/Project9-MultiModal-F2S-v2/1-automate.py
@@ -1,17 +1,19 @@
 import os
 import re
 
-# Path to your wav/image directory
-demo_dir = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/static/wavs/forDemo'
-output_html = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/index.html'
+# Define input list of IDs to include
+ids = ['2', '3', '4', '5', '9', '10', '13', '14', '15', '16', '18', '20', '21', '26', '29', '30', '35', '37', '38', '44', '47', '62', '69', '101','102', '103', '105', '107','108', '110', '111', '113','114', '115', '116', '118','119', '121', '122', '124']  # example list you can customize
+print(len(ids))
 
-# Collect all IDs based on existing GT audio
-pattern = re.compile(r'gt-(\d+)\.wav')
-ids = sorted([
-    pattern.match(f).group(1)
-    for f in os.listdir(demo_dir)
-    if pattern.match(f)
-], key=lambda x: int(x))
+# Define paths for each model and ground truth
+pluster_dir = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/static/wavs/1-pluster-baseline-350000'
+facetts_dir = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/static/wavs/1-facetts-naver'
+fvtts_dir = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/static/wavs/1-fvtts-interspeech2024'
+proposed_dir = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/static/wavs/3-6-DoubleClassifier-2attGenderRace-0.3-350000'
+gt_audio_dir = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/static/wavs/0-gt_audio'
+gt_image_dir = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/static/wavs/0-gt_images'
+
+output_html = '/home/jeonyj0612/yjswork.github.io/demos/Project9-MultiModal-F2S-v2/index.html'
 
 html_head = '''<!DOCTYPE html>
 <html>
@@ -45,10 +47,25 @@ html_tail = '''
 part1_section = ''
 part2_section = ''
 
+def find_file_with_id(directory, id):
+    pattern = re.compile(rf'^{id}(?:_.*)?\.wav$')
+    for f in os.listdir(directory):
+        if pattern.match(f):
+            return f
+    print(f"[Warning] ID {id} not found in {directory}")
+    return None
+
 for i, id in enumerate(ids):
+    gt_audio = find_file_with_id(gt_audio_dir, id)
+    pluster_audio = find_file_with_id(pluster_dir, id)
+    facetts_audio = find_file_with_id(facetts_dir, id)
+    fvtts_audio = find_file_with_id(fvtts_dir, id)
+    proposed_audio = find_file_with_id(proposed_dir, id)
+    gt_image = f'image-{id}.jpg'
+
     part1_section += f'''
     <div class="columns is-centered">
-      <div class="column is-four-fifths has-text-right">
+      <div class="column is-four-fifths has-text-centered">
         <h4 class="title is-4">Part 1 - Case {i+1}</h4>
       </div>
     </div>
@@ -57,25 +74,25 @@ for i, id in enumerate(ids):
         <div class="content">
           <p><b>Ground Truth</b></p>
           <audio controls>
-            <source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/gt-{id}.wav" type="audio/wav">
+            <source src="{gt_audio_dir}/{gt_audio}" type="audio/wav">
           </audio>
         </div><br>
         <div class="columns is-centered">
           <div class="column content has-text-centered">
             <p><b>Model-A</b></p>
-            <audio controls><source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/pluster-{id}.wav" type="audio/wav"></audio>
+            <audio controls><source src="{pluster_dir}/{pluster_audio}" type="audio/wav"></audio>
           </div>
           <div class="column content has-text-centered">
             <p><b>Model-B</b></p>
-            <audio controls><source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/FaceTTS-{id}.wav" type="audio/wav"></audio>
+            <audio controls><source src="{facetts_dir}/{facetts_audio}" type="audio/wav"></audio>
           </div>
           <div class="column content has-text-centered">
             <p><b>Model-C</b></p>
-            <audio controls><source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/FVTTS-{id}.wav" type="audio/wav"></audio>
+            <audio controls><source src="{fvtts_dir}/{fvtts_audio}" type="audio/wav"></audio>
           </div>
           <div class="column content has-text-centered">
             <p><b>Model-D</b></p>
-            <audio controls><source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/proposed-{id}.wav" type="audio/wav"></audio>
+            <audio controls><source src="{proposed_dir}/{proposed_audio}" type="audio/wav"></audio>
           </div>
         </div>
       </div>
@@ -84,7 +101,7 @@ for i, id in enumerate(ids):
 
     part2_section += f'''
     <div class="columns is-centered">
-      <div class="column is-four-fifths has-text-right">
+      <div class="column is-four-fifths has-text-centered">
         <h4 class="title is-4">Part 2 - Case {i+1}</h4>
       </div>
     </div>
@@ -92,24 +109,24 @@ for i, id in enumerate(ids):
       <div class="column is-four-fifths">
         <div class="content">
           <p><b>Ground Truth Image</b></p>
-          <img src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/image-{id}.jpg" width="200">
+          <img src="{gt_image_dir}/{gt_image}" width="200">
         </div><br>
         <div class="columns is-centered">
           <div class="column content has-text-centered">
             <p><b>Model-A</b></p>
-            <audio controls><source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/pluster-{id}.wav" type="audio/wav"></audio>
+            <audio controls><source src="{pluster_dir}/{pluster_audio}" type="audio/wav"></audio>
           </div>
           <div class="column content has-text-centered">
             <p><b>Model-B</b></p>
-            <audio controls><source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/FaceTTS-{id}.wav" type="audio/wav"></audio>
+            <audio controls><source src="{facetts_dir}/{facetts_audio}" type="audio/wav"></audio>
           </div>
           <div class="column content has-text-centered">
             <p><b>Model-C</b></p>
-            <audio controls><source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/FVTTS-{id}.wav" type="audio/wav"></audio>
+            <audio controls><source src="{fvtts_dir}/{fvtts_audio}" type="audio/wav"></audio>
           </div>
           <div class="column content has-text-centered">
             <p><b>Model-D</b></p>
-            <audio controls><source src="/demos/Project9-MultiModla-F2S-v2/wavs/forDemo/proposed-{id}.wav" type="audio/wav"></audio>
+            <audio controls><source src="{proposed_dir}/{proposed_audio}" type="audio/wav"></audio>
           </div>
         </div>
       </div>
